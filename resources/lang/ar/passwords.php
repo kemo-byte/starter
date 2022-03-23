900c603bbbd8f857d0f7faed99b0d5ec7ffaed7b
kemo-byte <kamalkafi12@gmail.com>
2022-03-23 21:18:29 +0200

    add languages and translate

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'تم تغيير كلمة المرور',
+    'sent' => 'أرسلنا لك رابط تغيير كلمة المرور في بريدك الإلكتروني',
+    'throttled' => 'إنتظر قليلاً قبل إعادة المحاولة',
+    'token' => 'هذا الرمز غير صالح',
+    'user' => "لا يوجد مستخدم بهذا البريد الإلكتروني",
+
+];
