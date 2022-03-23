e4de7e6bcec636638ca4f7e33e5308e17a246a11
kemo-byte <kamalkafi12@gmail.com>
2022-03-23 20:31:31 +0200

    namespace and prefix and grouping

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SecondConroller;
 
 Route::get('/admin', function () {
     return 'admin';
 });
+
+Route::group(['namespace'=>'Admin','prefix'=>'a'], function () {
+    
+    Route::get('second','SecondConroller@show');
+});
+
