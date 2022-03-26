06b11e387c3b0d0213baf7315278081d21195eff
kemo-byte <kamalkafi12@gmail.com>
2022-03-26 23:37:05 +0200

    schedule for user subscription expiration => php artisan make:command schedule name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('expire')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
